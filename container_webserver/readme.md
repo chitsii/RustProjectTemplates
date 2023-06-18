7c74a53ceeaca88ca61cad5327e88e3713afcc2d
FanJiang <tishimid.congianca@gmail.com>
2023-06-18 13:56:04 +0900

    update routing example

--- container_webserver/readme.md
+++ container_webserver/readme.md
@@ -2,6 +2,10 @@
 ```bash
 docker build . -t rust_server 
 docker run -p 3000:3000 -t rust_server
-curl http://localhost:3000
-    > <h1>Hello, World!</h1>⏎
+curl -i http://localhost:3000
+    # HTTP/1.1 200 OK
+    # content-type: text/html; charset=utf-8
+    # content-length: 11
+    # date: Sun, 18 Jun 2023 04:28:51 GMT
+    # <h1>OK</h1>⏎    
 ```
--- container_webserver/src/main.rs
+++ container_webserver/src/main.rs
@@ -1,12 +1,27 @@
-use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::{hash::Hash, net::SocketAddr};
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::{Extension, Path},
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Json, Router,
+};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
-    let port = std::env::var("PORT").ok().and_then(|p| p.parse().ok()).unwrap_or(3000);
+    let port = std::env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(3000);
     let address = SocketAddr::from(([0, 0, 0, 0], port));
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/hc", get(health_check))
+        .route("/echo/:id", get(return_value));
 
     axum::Server::bind(&address)
         .serve(app.into_make_service())
@@ -14,6 +29,20 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+pub async fn root() -> Html<&'static str> {
+    Html("<h1>OK</h1>")
+}
+
+pub async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
+}
+
+pub async fn return_value(Path(id): Path<String>) -> Result<impl IntoResponse, StatusCode> {
+    let res: HashMap<String, String> = HashMap::from_iter([("id".to_string(), id)]);
+
+    if res.get("id").unwrap() == "error" {
+        Err(StatusCode::BAD_REQUEST)
+    } else {
+        Ok((StatusCode::OK, Json(res)))
+    }
 }
